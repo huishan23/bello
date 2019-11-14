881bf44dcafeb97c137b4c0648f1e1e912b6ccd2
huishan23 <56948546+huishan23@users.noreply.github.com>
2019-11-14 14:25:22 +0800

    Add files via upload

---
+++ From1.vb
@@ -0,0 +1,31 @@
+﻿Public Class From1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim country As String
+        Dim capital As String
+
+        country = TextBox1.Text
+
+        Select Case country
+
+            Case "Malaysia"
+                capital = "Kuala Lumpur"
+            Case "Thailand"
+                capital = "Bangkok"
+            Case "Filipina"
+                capital = "Manila"
+            Case Else
+                capital = "unknown"
+        End Select
+
+        TextBox2.Text = capital
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox1.Focus()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        End
+    End Sub
+End Class
